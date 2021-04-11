0173b996588d21150bd89ac1881eabfe0d614cc9
Eileen <eileenlanda@icloud.com>
2021-04-11 08:24:10 -0500

    New Component

--- FrontEnd/your-movie-app/src/components/FocusMovie.js
+++ FrontEnd/your-movie-app/src/components/FocusMovie.js
@@ -1,21 +1,23 @@
 import axios from "axios";
 import {useEffect, useState} from 'react'
 
-const FocusMovie = ({key, movieTitle, src}) => {
+const FocusMovie = ({keyValue, movieTitle, src}) => {
 
     const [score, setScore] = useState("")
 
     const fetchNLPDataFromAPI = () => {
-        axios.get("https://gaz3000-auburnhacks21-app.herokuapp.com/get_sentiment/" + key)
+        axios.get("https://gaz3000-auburnhacks21-app.herokuapp.com/get_sentiment/" + keyValue)
         .then((res) => {
             setScore(res.data.score)
+            console.log("click")
+            console.log(res.data.score)
         })
 
     }
 
     return (
         <div onClick={() => fetchNLPDataFromAPI()}>
-            <p key={key}>{movieTitle}</p>
+            <p key={keyValue}>{movieTitle}</p>
         <img src={src} alt={movieTitle}/>
         <h2>{score}</h2>
         </div>
